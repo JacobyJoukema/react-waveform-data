326c0e7e18f6d837c513cd382e9f6523aeb7070e
Jacoby Joukema <jacoby.joukema@gmail.com>
2020-02-12 18:01:11 -0500

    Blank component class

---
+++ src/Waveform/Waveform.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export interface WaveformProps {
+}
+
+export interface WaveformState {
+}
+
+export class Waveform extends Component<WaveformProps, WaveformState>  {
+
+    constructor(props: WaveformProps) {
+        super(props);
+
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <>Insert Component Here</>
+        )
+
+    }
+}
